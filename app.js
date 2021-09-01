e8d4f9a5b3420c37169b817230db94a86d4dc6bd
octaviotastico <octaviopercivaldi2@gmail.com>
2021-09-01 03:07:21 -0300

    Socket connection working!!! Connected with DTN-Backend

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 // Library Imports
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
-// App creation
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors())
 
 // Database Connection
 mongoose.connect('mongodb://localhost:27017/cms-db', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
@@ -15,12 +10,33 @@ mongoose.connect('mongodb://localhost:27017/cms-db', { useNewUrlParser: true, us
 });
 
 
-app.use('/aap', require('./src/routes/aap'));
+// App setup
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors())
+
+
+// Routes
 app.use('/calendar', require('./src/routes/calendar'));
 app.use('/learning', require('./src/routes/learning'));
 app.use('/people', require('./src/routes/people'));
 
-// App listening to port 2424
+
+// Listen for HTTP requests on port 2424
 app.listen(2424, () => {
   console.log('CMS up and runnig!! 🕺🕺🕺');
 });
+
+
+// Socket io client setup
+const io = require("socket.io-client")
+const ioClient = io.connect("http://localhost:7575");
+
+
+// (Move to a separate file)
+ioClient.on("message", (data) => {
+  console.log("Message received from backend:", data);
+});
+
+ioClient.emit("message", "Hello from CMS backend!");
