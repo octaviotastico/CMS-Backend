731e414529095b6f1bb2a015e3eeb5e910712cd0
Octavio Percivaldi <octaviopercivaldi2@gmail.com>
2022-04-23 03:09:14 -0300

    Added new functions to fielSystem

--- src/commons/fileSystem.js
+++ src/commons/fileSystem.js
@@ -1,21 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 const os = require("os");
 
 const writeLineToFile = (text, filename) => {
   filename = `./data/${filename}`;
-  fs.appendFileSync(filename, `${text}${os.EOL}`, err => {
+  fs.appendFileSync(filename, `${text}${os.EOL}`, (err) => {
     if (err) return console.error(err);
   });
-}
+};
 
 const readEntireFile = (filename) => {
   filename = `./data/${filename}`;
   const fileExists = fs.existsSync(filename);
-  if (fileExists) return fs.readFileSync(filename, 'utf8');
-  return '';
+  if (fileExists) return fs.readFileSync(filename, "utf8");
+  return "";
+};
+
+// Check if a directory exists
+const directoryExists = (path) => {
+  try {
+    return fs.statSync(path).isDirectory();
+  } catch (err) {
+    return false;
+  }
+};
+
+// Create the directory if it doesn't exist
+const createDirectory = (path) => {
+  if (!directoryExists(path)) {
+    fs.mkdirSync(path);
+  }
 };
 
 module.exports = {
   writeLineToFile,
-  readEntireFile
+  readEntireFile,
+  directoryExists,
+  createDirectory,
 };
